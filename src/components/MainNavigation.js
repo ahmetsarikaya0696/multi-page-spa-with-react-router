f0969ebfb27e524e9212c980bdaf1b1922f329a0
ahmet sarikaya <ahmetsarikaya0696@outlook.com>
2023-07-24 15:37:00 +0300

    routing, link, navlink, layout, showing error pages, navigating programmaticly, dynamic routes, relative and absolute paths

---
+++ src/components/MainNavigation.js
@@ -0,0 +1,31 @@
+import { NavLink } from "react-router-dom";
+import styles from "./MainNavigation.module.css";
+
+const MainNavigation = () => {
+  return (
+    <header className={styles.header}>
+      <nav>
+        <ul className={styles.list}>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) => isActive && styles.active}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/products"
+              className={({ isActive }) => isActive && styles.active}
+            >
+              Products
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigation;
